a8c2abaeaf5aa03801dd82cdc264c2cf4c58e4d2
Nidhin <ldnidhin@gmail.com>
2026-08-13 21:17:14 +0530

    chore: ignore production database and images

--- test-scraper.js
+++ test-scraper.js
@@ -1,13 +1,15 @@
 const cheerio = require('cheerio');
 const url = 'https://link.amazon/B0dkzAqso';
 const scraperApiKey = '17b71d086ce0859b314187fdbd489745';
-const fetchUrl = `http://api.scraperapi.com?api_key=${scraperApiKey}&url=${encodeURIComponent(url)}`;
+let finalUrlToScrape = url;
 
-fetch(fetchUrl)
-.then(r => r.text())
-.then(html => {
-  const $ = cheerio.load(html);
-  const possiblePrices = $('.a-price-whole, .a-price, .apexPriceToPay, .priceToPay, [class*="price"]').map((_, el) => $(el).text()).get();
-  console.log('Got HTML length:', html.length, 'Prices found:', possiblePrices.slice(0, 10));
-})
-.catch(console.error);
+fetch(url, { method: 'HEAD', redirect: 'follow' }).then(res => {
+  if (res.url) finalUrlToScrape = res.url;
+  const fetchUrl = `http://api.scraperapi.com?api_key=${scraperApiKey}&url=${encodeURIComponent(finalUrlToScrape)}`;
+  console.log('Final URL:', finalUrlToScrape);
+  return fetch(fetchUrl);
+}).then(r => r.text()).then(html => {
+  console.log('Got HTML length:', html.length);
+  if (html.includes('id="productTitle"')) console.log('SUCCESS! Found Product Title.');
+  else console.log('HTML preview:', html.substring(0, 500));
+}).catch(console.error);
